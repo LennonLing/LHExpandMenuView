4633ae19887faca0770e88432a56be67565b9489
lingbin <bin.ling.2018@gmail.com>
2019-03-25 16:49:40 +0800

    first commit

---
+++ LHExpandMenuView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHExpandMenuView
+//
+//  Created by 凌斌 on 2019/3/25.
+//  Copyright © 2019 lingbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHExpandMenuView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LHExpandMenuView
+//
+//  Created by 凌斌 on 2019/3/25.
+//  Copyright © 2019 lingbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
